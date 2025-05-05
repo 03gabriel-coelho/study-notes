8dc5f1004d21dc4fc23eaef20ef6412cb337b0e0
Gabriel Henrique Coelho Freitas <coelho.tibbers@gmail.com>
2025-05-04 22:35:50 -0300

    feat: adicionar modelo SiteContato, migração e rotas, além de atualizar views para usar rotas nomeadas

--- api/laravel/anotacoes.php
+++ api/laravel/anotacoes.php
@@ -1,6 +1,19 @@
+COMPOSER:
+
+composer create-project --prefer-dist laravel/laravel nome_projeto "8.*" ----- > criar projeto laravel 8
+
 php artisan list -> Ver lista de comandos do artisan
 
+php artisan migrate ----- > criar as tabelas do banco de dados
 php artisan serve  -----
 php -S localhost:8000 ----- > iniciar servidor local
 php artisan make:controller SobreNosController ----- > criar controller
 php artisan route:list ----- > listar rotas
+php artisan make:model SiteContato ------> criar model
+php artisan make:model SiteContato -m ------> criar model e migration juntos
+php artisan make:seeder SiteContatoSeeder ------> criar seeder
+php artisan make:model -mcr SiteContato ------> criar model, migration e controller juntos
+
+DOCUMENTAÇÃO:
+Visualizar as colunas
+https://laravel.com/docs/11.x/migrations#column-method-boolean
--- api/laravel/app_super_gestao/routes/web.php
+++ api/laravel/app_super_gestao/routes/web.php
@@ -4,9 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SobreNosController as TesteController;
 use App\Http\Controllers\ContatoController;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [TesteController::class, 'sobreNos']);
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [TesteController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('site.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('site.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos';})->name('site.produtos');
+});
 
 // ---EXEMPLO--- Conseguimos passar parametros desta forma, não precisa ter a mesma nomenclatura
 // Route::get('/contato/{nome}/{y}', function (string $xyz, string $z) {
@@ -44,4 +51,18 @@ Route::get(
 )
 ->where('categoria_id', '[0-9]+') // [0-9]+ - só aceita números
 ->where('nome', '[A-Za-z]+'); // [0-9]+ - só aceita letras
+
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1'); // redireciona a rota 2 para a rota 1
+})->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1'); // redireciona a rota 2 para a rota 1
+
+Route::fallback(function(){ // CRIANDO UMA ROTA FALLBACK PARA REDIRECIONAR QUANDO A ROTA NÃO EXISTE
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para voltar a página inicial.';
+});
 
