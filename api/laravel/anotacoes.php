f7d04b76d15e6d56a0b0f421421810e575195791
Gabriel Henrique Coelho Freitas <94490310+03gabriel-coelho@users.noreply.github.com>
2025-05-05 17:28:38 -0300

    feat: atualizar anotações com novos comandos Artisan e documentação de debug

--- api/laravel/anotacoes.php
+++ api/laravel/anotacoes.php
@@ -5,8 +5,12 @@ composer create-project --prefer-dist laravel/laravel nome_projeto "8.*" ----- >
 php artisan list -> Ver lista de comandos do artisan
 
 php artisan migrate ----- > criar as tabelas do banco de dados
+php artisan db:seed ----- > popular o banco de dados com os dados do seeder
+php artisan key:generate ----- > gerar a chave de segurança do projeto
+
 php artisan serve  -----
 php -S localhost:8000 ----- > iniciar servidor local
+
 php artisan make:controller SobreNosController ----- > criar controller
 php artisan route:list ----- > listar rotas
 php artisan make:model SiteContato ------> criar model
@@ -14,6 +18,14 @@ php artisan make:model SiteContato -m ------> criar model e migration juntos
 php artisan make:seeder SiteContatoSeeder ------> criar seeder
 php artisan make:model -mcr SiteContato ------> criar model, migration e controller juntos
 
+php artisan config:cache ----- > limpar cache de configuração
+
 DOCUMENTAÇÃO:
 Visualizar as colunas
 https://laravel.com/docs/11.x/migrations#column-method-boolean
+
+DEBUGANDO:
+dd($variavel) ----- > imprime a variável e para a execução do código
+dump($variavel) ----- > imprime a variável e continua a execução do código
+print_r($variavel) ----- > imprime a variável e continua a execução do código
+var_dump($variavel) ----- > imprime a variável e continua a execução do código
